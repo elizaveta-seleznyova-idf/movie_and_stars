a4665f0281ef7e210a5f94cf81500426b5f7ff6f
elizaveta-seleznyova-idf <elizaveta.seleznyova@idfinance.com>
2022-11-11 13:04:49 +0300

    fixes

--- packages/presentation/lib/config/responsive/responsive.dart
+++ packages/presentation/lib/config/responsive/responsive.dart
@@ -8,19 +8,4 @@ class Responsive {
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width < Dimens.size500;
 
-
-  static int crossAxisCountSize(BuildContext context){
-    final double screenWidth = MediaQuery.of(context).size.width;
-    if (screenWidth > Dimens.size1200) {
-      return 6;
-    } else if (screenWidth > Dimens.size1000) {
-      return 5;
-    } else if (screenWidth > Dimens.size800) {
-      return 4;
-    } else if (screenWidth > Dimens.size600) {
-      return 3;
-    } else {
-      return 2;
-    }
-  }
 }
